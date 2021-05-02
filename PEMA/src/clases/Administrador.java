7a6b119268967fbe54286d88cece98e21d67d980
saraviarc <73924032+saraviarc@users.noreply.github.com>
2021-05-02 14:48:02 -0600

    Creación de clase Administrador

---
+++ PEMA/src/clases/Administrador.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author Roberto Mendoza
+ */
+public class Administrador {
+    //Atributos
+    private String nombre="admin";
+    private String idUsuario="admin";
+    private String contraseña="admin";
+    
+    //Método constructor por defecto
+    public Administrador(){
+    };
+    
+    //Métodos
+    public void registrarEmpleado(){
+    }
+    
+    public void eliminarEmpleado(){
+    }
+}
