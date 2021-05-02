71c88444fcac2cfb3548fac4e4b5b5fd605dfa40
saraviarc <73924032+saraviarc@users.noreply.github.com>
2021-05-02 13:00:55 -0600

    Creación de la clase principal

---
+++ PEMA/src/pema/PEMA.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pema;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Roberto Mendoza
+ */
+public class PEMA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        JOptionPane.showMessageDialog(null, "Hello Word!");
+    }
+    
+}
